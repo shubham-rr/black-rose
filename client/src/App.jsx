dc0cfcb66dd0d9af3bc77115bc127d810602acb2
Gilbert <gilbertofer.tanoto01@gmail.com>
2025-01-06 16:25:10 +0930

    gilbs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,53 +1,20 @@
-// import { useState } from 'react'
-// import './App.css'
-// import Header from './components/Header/Header'
-// import Footer from './components/Footer/Footer'
-// import Hero from './components/Hero/Hero'
-// import FeaturedProducts from './components/FeaturedProducts/FeaturedProducts'
-
-// function App() {
-
-//   return (
-//     <div className="page-container">
-//       <Header />
-//       <Hero title="ALL THINGS CAMERAS" subtitle="Capture Life's Moments with Professional Grade Equipment" />
-//       <FeaturedProducts />
-//       {/* Your other components */}
-//       <Footer />
-//     </div>
-//   )
-// }
-
-// export default App
-
 import { useState } from 'react'
 import './App.css'
-import{ Routes, Route} from 'react-router-dom'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Hero from './components/Hero/Hero'
 import FeaturedProducts from './components/FeaturedProducts/FeaturedProducts'
-import ShoppingCart from './pages/ShoppingCart/ShoppingCart.jsx'
 
 function App() {
+
   return (
     <div className="page-container">
       <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/cart" element={<ShoppingCart />} />
-      </Routes>
-      <Footer />
-    </div>
-  )
-}
-
-function HomePage() {
-  return (
-    <>
       <Hero title="ALL THINGS CAMERAS" subtitle="Capture Life's Moments with Professional Grade Equipment" />
       <FeaturedProducts />
-    </>
+      {/* Your other components */}
+      <Footer />
+    </div>
   )
 }
 
