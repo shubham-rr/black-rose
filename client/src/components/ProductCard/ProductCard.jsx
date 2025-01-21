605945f26eb545cc7f73d44ec51f4edf94f19fb9
Shubham Maharjan <shubham.maharjan.2017@gmail.com>
2025-01-21 15:11:00 +0930

    feat: add product detail section

--- client/src/components/ProductCard/ProductCard.jsx
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -1,58 +1,40 @@
 import React from "react";
 import { Button } from 'react-bootstrap';
 import defaultCamera from "../../assets/brand-logos/default.png";
-import CanonLogo from "../../assets/brand-logos/Canon.svg";
-import NikonLogo from "../../assets/brand-logos/Nikon.jpg";
-import FujifilmLogo from "../../assets/brand-logos/Fujifilm.png";
-import SonyLogo from "../../assets/brand-logos/Sony.svg";
-import PanasonicLogo from "../../assets/brand-logos/Panasonic.svg";
-import OlympusLogo from "../../assets/brand-logos/Olympus.svg";
-import LeicaLogo from "../../assets/brand-logos/Leica.jpg";
-import DJILogo from "../../assets/brand-logos/DJI.png";
-import GoProLogo from "../../assets/brand-logos/GoPro.png"
-
+import logos from "../../assets/brand-logos/brand-logos";
+import { Link } from 'react-router-dom';
 
 import "./ProductCard.css";
 
 const ProductCard = ({ product }) => {
   const getBrandLogo = (brand) => {
-    const logos = {
-      Canon: CanonLogo,
-      Nikon: NikonLogo,
-      Fujifilm: FujifilmLogo,
-      Sony: SonyLogo,
-      Panasonic: PanasonicLogo,
-      Olympus: OlympusLogo,
-      Leica: LeicaLogo,
-      GoPro: GoProLogo,
-      DJI : DJILogo,
-
-    };
     return logos[brand.charAt(0).toUpperCase() + brand.slice(1).toLowerCase()] || null;
   };
 
   return (
     <div className="product-card">
-      <div className="product-content">
-        <div className="product-image">
-          <img
-            src={product.image || defaultCamera}
-            alt={product.name}
-            onError={(e) => {
-              e.target.onerror = null;
-              e.target.src = defaultCamera;
-            }}
-          />
-        </div>
-        <div className="product-details">
-          <div className="product-brand-logo">
-            {getBrandLogo(product.brand) && (
-              <img src={getBrandLogo(product.brand)} alt={product.brand} />
-            )}
+      <Link to={`/product/${product.id}`}>
+        <div className="product-content">
+          <div className="product-image">
+            <img
+              src={product.image || defaultCamera}
+              alt={product.name}
+              onError={(e) => {
+                e.target.onerror = null;
+                e.target.src = defaultCamera;
+              }}
+            />
           </div>
-          <p className="product-name">{product.name}</p>
+          <div className="product-details">
+            <div className="product-brand-logo">
+              {getBrandLogo(product.brand) && (
+                <img src={getBrandLogo(product.brand)} alt={product.brand} />
+              )}
+            </div>
+            <p className="product-name">{product.name}</p>
+          </div>
         </div>
-      </div>
+      </Link>
       <div className="product-footer">
         <p className="product-price">${product.price.toFixed(2)}</p>
         <Button variant="dark" size="sm" className="action-button">
