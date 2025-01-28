2f42959162782e3bb832b4d599ab46e813faf116
abedcodes13 <114854246+abedscodes@users.noreply.github.com>
2025-01-28 16:24:52 +0930

    backend init

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+﻿import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+// App Config
+
+const app = express()
+const port = process.env.PORT || 4000
+
+// Middlewares
+app.use(express.json())
+app.use(cors())
+
+// API endpoints
+
+app.get('/',(req,res)=>{
+    res.send("API Working")
+})
+
+app.listen(port, ()=> console.log('Server started on PORT: ' + port))
