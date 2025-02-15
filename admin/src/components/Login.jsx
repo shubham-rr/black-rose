c848ea36183f65905876b1284a791e54a78ecc59
abedcodes13 <114854246+abedscodes@users.noreply.github.com>
2025-02-15 21:04:46 +0930

    admin

---
+++ admin/src/components/Login.jsx
@@ -0,0 +1,24 @@
+﻿import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <div>
+        <h1>Admin Panel</h1>
+        <form >
+            <div className='mb-3 min-w-72'>
+                <p className='text-sm font-medium text-gray-700 mb-2'>Email Address</p>
+                <input className='rounded-md w-full px-3 py-3 border border-gray-300 outline-none' type="email" placeholder='your@email.com' required/>
+            </div>
+            <div className='mb-3'>
+                <p className='text-sm font-medium text-gray-700 mb-2'>Password</p>
+                <input className='rounded-md w-full px-3 py-3 border border-gray' type="password" placeholder='Enter your password.' required/>
+            </div>
+            <button type='submit'> Login </button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Login
--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -2,9 +2,9 @@
 import {assets} from '../assets/assets'
 const Navbar = () => {
   return (
-    <div>
-      <img src={assets.logo} alt="" />
-      <button>Logout</button>
+    <div className='flex items-center py-2 px-[4%] justify-between'>
+      <img className='w-[max(10%,80px)]' src={assets.logo} alt="" />
+      <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:txt-sm'>Logout</button>
     </div>
   )
 }
