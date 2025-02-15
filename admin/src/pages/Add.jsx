f5084eee150d292e93dd5c0ce8193ba0843274a2
abedcodes13 <114854246+abedscodes@users.noreply.github.com>
2025-02-15 23:50:36 +0930

    admin page updated

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,10 +1,125 @@
-﻿import React from 'react'
+﻿import React, { useState } from 'react'
+import { assets } from '../assets/assets'
 
 const Add = () => {
+
+  const [image1,setImage1] = useState(false)
+  const [image2,setImage2] = useState(false)
+  const [image3,setImage3] = useState(false)
+  const [image4,setImage4] = useState(false)
+
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState('');
+  const [category, setCategory] = useState('Camera');
+  const [subCategory, setSubCategory] = useState('Other');
+  const [bestSeller, setBestSeller] = useState(false);
+  const [sizes,setSizes] = useState([]);
+  
+
   return (
-    <div>
+    <form className='flex flex-col w-full items-start gap-3'>
+      <div>
+        <p className='mb-2 '>Upload Image</p>
+
+        <div className='flex gap-2'>
+          <label htmlFor="image1">
+            <img className='w-20' src={assets.upload_area} alt="" />
+            <input type="file" id="image1" hidden />
+          </label>
+
+          <label htmlFor="image2">
+            <img className='w-20' src={assets.upload_area} alt="" />
+            <input type="file" id="image2" hidden />
+          </label>
+
+          <label htmlFor="image3">
+            <img className='w-20' src={assets.upload_area} alt="" />
+            <input type="file" id="image3" hidden />
+          </label>
+
+          <label htmlFor="image4">
+            <img className='w-20' src={assets.upload_area} alt="" />
+            <input type="file" id="image4" hidden />
+          </label>
+        </div>
+      </div>
       
-    </div>
+      <div className='w-full'>
+        <p className='mb-2'>Product Name</p>
+        <input className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Type here' required/>
+      </div>
+
+      <div className='w-full'>
+        <p className='mb-2'>Product Description</p>
+        <textarea className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Type description' required/>
+      </div>
+
+      <div className='flex flex-col sm:flex-row gap-2 w-full sm:gap-8'>
+        <div>
+          <p className='mb-2'>Product Category</p>
+          <select className='w-full px-3 py-2'>
+            <option value="Camera">Camera</option>
+            <option value="Drone">Drone</option>
+            <option value="Accessories">Lens</option>
+          </select>
+        </div>
+
+        <div>
+          <p className='mb-2'>Sub-Category</p>
+          <select className='w-full px-3 py-2'>
+            <option value="Bags">Bags</option>
+            <option value="Lens">Lens</option>
+            <option value="Other">Other</option>
+          </select>
+        </div>
+
+        {/* <div>
+          <p className='mb-2'>Brand</p>
+          <select >
+            <option value="Canon">Canon</option>
+            <option value="Nikon">Nikon</option>
+            <option value="DJI">DJI</option>
+            <option value="Other">Other</option>
+          </select>
+        </div> */}
+      </div>
+
+      <div>
+        <p className='mb-2'>Product Price</p>
+        <input className='w-full px-3 py-2 sm:w-[120px]' type="number" />
+      </div>
+
+      <div>
+        <p className='mb-2'>Product Sizes</p>
+        <div className='flex gap-3'>
+          <div>
+            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>S</p>
+          </div>
+          
+          <div>
+            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>M</p>
+          </div>
+
+          <div>
+            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>L</p>
+          </div>
+
+          <div>
+            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XL</p>
+          </div>
+        </div>
+      </div>
+
+      <div className='flex gap-2 mt-2'>
+        <input type="checkbox" id="bestseller" />
+        <label className='cursor-pointer' htmlFor="bestseller">Add to Best Seller</label> 
+      </div>
+
+
+      <button type="submit" className='w-28 py-3 mt-4 bg-black text-white'>Add</button>
+
+    </form>
   )
 }
 
