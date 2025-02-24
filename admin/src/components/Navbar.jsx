e7dd79acb12fefb87dd06cb27a688a364833f80f
Shubham Maharjan <shubham.maharjan.2017@gmail.com>
2025-02-24 14:14:39 +0930

    admin: update navbar color and size

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import {assets} from '../assets/assets'
 
 const Navbar = ({setToken}) => {
   return (
-    <div className='flex items-center py-2 px-[4%] justify-between'>
+    <div className='flex items-center py-8 px-[4%] justify-between bg-black'>
       <img className='w-[max(10%,80px)]' src={assets.logo} alt="" />
-      <button onClick={()=>setToken('')} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:txt-sm'>Logout</button>
+      <button onClick={()=>setToken('')} className='bg-gray-100 text-black px-5 py-2 sm:px-7 sm:py-2 rounded-full text-sm sm:txt-sm'>Logout</button>
     </div>
   )
-}
+} 
 
 export default Navbar
