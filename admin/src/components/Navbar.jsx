f187b49bc0874e5f8f8a225fd19ccce659876a82
Shubham Maharjan <shubham.maharjan.2017@gmail.com>
2025-02-25 10:26:53 +0930

    admin: update logo and navbar to white scheme

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import {assets} from '../assets/assets'
 
 const Navbar = ({setToken}) => {
   return (
-    <div className='flex items-center py-8 px-[4%] justify-between bg-black'>
-      <img className='w-[max(10%,80px)]' src={assets.logo} alt="" />
-      <button onClick={()=>setToken('')} className='bg-gray-100 text-black px-5 py-2 sm:px-7 sm:py-2 rounded-full text-sm sm:txt-sm'>Logout</button>
+    <div className='flex items-center py-8 px-[4%] justify-between'>
+      <img className='w-[max(15%,80px)]' src={assets.logo} alt="" />
+      <button onClick={()=>setToken('')} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-sm sm:txt-sm'>Logout</button>
     </div>
   )
 } 
