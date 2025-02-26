e926223278ca35e2bd9c42cea6ecc4893698fb89
Shubham Maharjan <128051227+shubham-rr@users.noreply.github.com>
2025-02-26 11:07:57 +0930

    Revert "Admin and Backend additions"

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,9 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import ShopContextProvider from "./context/ShopContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <ShopContextProvider>
-      <App />
-    </ShopContextProvider>
+    <App />
   </BrowserRouter>
 );
